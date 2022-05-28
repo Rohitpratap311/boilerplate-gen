5ffcff63967c28fdec85f209e3018d2b67bc0ed1
Dhiraj Temkar <46560643+Dhirajtemkar@users.noreply.github.com>
2022-05-28 09:48:17 +0530

    Update webappDocs.js

--- app/frontend/src/components/WebApp/webappDocs.js
+++ app/frontend/src/components/WebApp/webappDocs.js
@@ -3,16 +3,42 @@ import React from 'react'
 function QandA() {
     return (
       <div style={{ textAlign: "center", padding: "40px" }}>
-        <h1 style={{ color: "Lightgreen" }}>QUESTION ANSWERING (Q.A.) MODELS</h1>
+        <h1 style={{ color: "Lightgreen" }}>Documentations</h1>
         
         <h4 style={{ textAlign: "left" }}>
         <ul>
-          <li><p>The architecture employed here is an efficient way of preprocessing the raw data before passing it to the Q.A. model, where the input data can be of any form (pdf, word, docs, images, txt, ppt, Xls, Etc.), but the goal remains the same, i.e., to work efficiently with almost any kind of data received from the user.</p></li>
-          <li><p>The more metadata available, the more accurate the analysis of the file's content will be. Let it be a scanned pdf or an image; architecture should still work well with the documents by carrying out Optical Character Recognition(O.C.R.), performing content analysis, and further translating the metadata. Therefore, to support maximum document types, extract the metadata from them, perform content analysis, translation, search engine indexing, Etc., the Apache toolkit, Tika, and a FOSS(free and open-source software), can be helpful.</p></li>
-          <li><p>Almost every Q.A. architecture based on transformers follows the same pipeline of breaking down the input text into paragraphs and tokenizing it. Further removes the stop words and then performs basic N.L.P. tasks (like lemmatization and stemming) and passes them to the transformer.</p></li>
-          <li><p>Given a document, assuming that not every sentence or paragraph is relevant to the query, not all metadata needs to follow the Q.A. pipeline. With the help of a probabilistic relevance model, the document can be filtered and eliminate a majority of the text by ranking texts/documents based on the relevance with a given search query. The Okapi(BM25), a weighting scheme framework, the best-known derivative of the probabilistic relevance model, can rank the text and efficiently query only the informative and relevant text.</p></li>
-          <li><p>A neural network-based deep learning model for performing N.L.P. tasks, trained on the S.Q.U.A.D.2.0 dataset, can accurately extract the selected content is required to answer. B.E.R.T. [13][15] (Bidirectional Encoder Representations from Transformers), A.L.B.E.R.T. (Acoustic and Laryngeal Biofeedback Enhancement Real-Time), E.L.E.C.T.R.A. (Efficiently Learning an Encoder that Classifies Token Replacements Accurately), Etc. are some of the widely accepted architecture designs for the Q.A. task.</p></li>
-          <li><p>The given architecture can be built on top of any existing Q.A. models to increase its efficiency.</p></li>
+          <li><h2>Main Repo:</h2></li>
+          <p>You can find more information about the main repository <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/README.md">here</a>.</p>
+
+          <br/>
+          <br/>
+          <li><h2>Frontend:</h2></li>
+
+          <p><h3>Angular</h3></p>
+          <p>You can find more information about the Angular <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/frontend/Angular/README.md">here</a>.</p>
+
+          <p><h3>React</h3></p>
+          <p>You can find more information about the React <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/frontend/React/README.md">here</a>.</p>
+
+          <p><h3>React Native</h3></p>
+          <p>You can find more information about the React Native <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/frontend/React/README.md">here</a>.</p>
+        
+          <br/>
+          <br/>
+          <li><h2>Backend:</h2></li>
+          
+          <p><h3>FastAPI</h3></p>
+          <p>You can find more information about the FastAPI <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/backend/FastAPI/Readme.md">here</a>.</p>
+
+          <p><h3>Flask</h3></p>
+          <p>You can find more information about the Flask <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/backend/Flask/README.md">here</a>.</p>
+
+          <p><h3>NodeJS</h3></p>
+          <p>You can find more information about the NodeJS <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/backend/Flask/README.md">here</a>.</p>
+
+          <p><h3>Spring Boot</h3></p>
+          <p>You can find more information about the Spring Boot <a style={{ color: "white" }} href="https://github.com/Aerothon-NTP/boilerplate-gen/blob/main/backend/Springboot/Readme.md">here</a>.</p>
+
         </ul>
         </h4>
       </div>
